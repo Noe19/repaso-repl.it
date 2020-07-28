9c30d3e773dd941663d1edf9d7fe6ad6f58340d8
denisse cumbal <denissecumbal2015@hotmail.com>
2020-07-28 00:38:00 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int  fac (int  );
+int main() {
+  int num;
+  cout<<"ingrese un numero";
+ cin>>num;
+cout << fac (num);
+
+return 0;
+}
+ int  fac (int num ){
+  
+
+ if (num==0){
+    return 1;
+ }else{
+   num=num*fac(num-1);
+   
+ }
+ 
+  return  num;
+ }
